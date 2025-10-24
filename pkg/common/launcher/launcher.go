7d0b28c92d95b7a079930fd08c57508f0f7cb21b
Jason Plein <jplein@gmail.com>
2025-10-24 15:35:26 -0700

    Put recent items first

--- pkg/common/launcher/launcher.go
+++ pkg/common/launcher/launcher.go
@@ -3,9 +3,12 @@ package launcher
 import (
 	"fmt"
 	"io"
+	"slices"
 	"strings"
 
+	"github.com/jplein/launchit/pkg/common/logger"
 	"github.com/jplein/launchit/pkg/common/source"
+	"github.com/jplein/launchit/pkg/common/state"
 )
 
 type Launcher struct {
@@ -22,6 +25,36 @@ func (l *Launcher) List() ([]source.Entry, error) {
 	entries, err := l.sources.List()
 	if err != nil {
 		return nil, fmt.Errorf("error listing entries: %w", err)
+	}
+
+	recents, err := state.Get()
+	if err != nil {
+		logger.Log("error reading recents: %v", err)
+	}
+
+	if recents != nil {
+		slices.SortFunc(entries, func(a, b source.Entry) int {
+			indexA := slices.Index(recents, a.ID)
+			indexB := slices.Index(recents, b.ID)
+
+			// Both found in recents: sort by index (lower index comes first)
+			if indexA != -1 && indexB != -1 {
+				return indexA - indexB
+			}
+
+			// Only a is in recents: a comes first
+			if indexA != -1 {
+				return -1
+			}
+
+			// Only b is in recents: b comes first
+			if indexB != -1 {
+				return 1
+			}
+
+			// Neither in recents: maintain original order
+			return 0
+		})
 	}
 
 	return entries, nil
