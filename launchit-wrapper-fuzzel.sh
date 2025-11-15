e35b6afbc8b963f5645e769a9348ce2524c278c5
Jason Plein <jplein@gmail.com>
2025-11-15 12:07:06 -0800

    Using /usr/bin/env bash, not #!/bin/bash - fix for NixOS

--- launchit-wrapper-fuzzel.sh
+++ launchit-wrapper-fuzzel.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 "$(dirname "$0")/launchit" | fuzzel --dmenu --with-nth=1 | "$(dirname "$0")/launchit" read
--- launchit-wrapper-rofi.sh
+++ launchit-wrapper-rofi.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 "$(dirname "$0")/launchit" write --columns=name,type --widths=69,11 | rofi -dmenu -normal-window -display-columns 1 | "$(dirname "$0")/launchit" read
