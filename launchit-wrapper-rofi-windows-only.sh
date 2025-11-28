35b0dadbbe0915451e1b4ce3c65cd2cb0d19f2c7
Jason Plein <jplein@gmail.com>
2025-11-28 15:36:51 -0800

    Use the copy of launchit put in the path by Nix

--- launchit-wrapper-rofi-windows-only.sh
+++ launchit-wrapper-rofi-windows-only.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-"$(dirname "$0")/launchit" write --source windows --columns=name,type --widths=69,11 | rofi -dmenu -display-columns 1 | "$(dirname "$0")/launchit" read
+launchit write --source windows --columns=name,type --widths=69,11 | rofi -dmenu -display-columns 1 | launchit read
